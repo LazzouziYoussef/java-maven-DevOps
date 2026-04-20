677f36cb33c4f47fe1150f71080fcf7a0b33c1fa
Youssef Lazzouzi <youssef.lazzouzi@etu.uae.ac.ma>
2026-04-20 16:27:57 +0100

    dummy change

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -1,10 +1,9 @@
 package maven;
 
 public class Hello {
-	
-	public static void main(String[] args)
-    {
-        System.out.println("Hello DevOps, Today is Monday afternoon V1.0...");
+
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps, Today is Monday afternoon V2026.0...");
     }
 
 }
